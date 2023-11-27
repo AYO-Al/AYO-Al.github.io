8d3d16cd7dfebc71248f12bbd1653512573c5ae7
AYO-AI <2609320892@qq.com>
2023-11-27 21:05:29 +0800

    Site updated: 2023-11-27 21:05:25

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/11/23/hello-world/","2023/11/23/title/","2023/11/23/Untitled/"];function toRandomPost(){
+var posts=["2023/11/23/Untitled/","2023/11/23/hello-world/","2023/11/23/title/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
